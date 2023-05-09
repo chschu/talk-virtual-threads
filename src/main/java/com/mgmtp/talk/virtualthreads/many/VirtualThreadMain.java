e7e8b0274c3ac04f7b78969ba3a46508ca34265c
Christian Schuster <christian.schuster@mgm-tp.com>
2023-05-09 10:58:17 +0200

    reduce number of threads for "many" example

--- src/main/java/com/mgmtp/talk/virtualthreads/many/VirtualThreadMain.java
+++ src/main/java/com/mgmtp/talk/virtualthreads/many/VirtualThreadMain.java
@@ -10,7 +10,7 @@ public class VirtualThreadMain {
 	public static void main(final String[] args) {
 		final Stopwatch stopwatch = Stopwatch.createStarted();
 		try (final ExecutorService executorService = Executors.newThreadPerTaskExecutor(Thread.ofVirtual().factory())) {
-			for (int i = 0; i < 50_000; i++) {
+			for (int i = 0; i < 20_000; i++) {
 				executorService.submit(DelayTask.INSTANCE);
 			}
 			System.out.println("Submit finished: " + stopwatch.elapsed().toMillis() + " ms");
